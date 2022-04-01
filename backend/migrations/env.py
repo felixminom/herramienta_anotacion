28b50b23ecfec848eb7478b9e91ec072259f9839
Felix Mino <fmino@stackbuilders.com>
2022-03-31 23:25:38 -0500

    Add back deleted line

--- backend/migrations/env.py
+++ backend/migrations/env.py
@@ -25,6 +25,7 @@ from flask import current_app
 config.set_main_option(
     'sqlalchemy.url', current_app.config.get(
         'SQLALCHEMY_DATABASE_URI').replace('%', '%%'))
+target_metadata = current_app.extensions['migrate'].db.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
